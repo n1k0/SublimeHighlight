b8b8fc053630b0ae7e8ac5e8961ea7ea77b27b76
Nicolas Perriault <nperriault@gmail.com>
2012-05-14 20:33:41 +0200

    added settings handling for theme and full html rendering, more to come

--- SublimeHighlight.py
+++ SublimeHighlight.py
@@ -2,7 +2,6 @@
 
 """
     TODO:
-    - add a setting to store prefered theme name
     - customize rendered HTML to ease choosing a new theme
 """
 
@@ -12,19 +11,19 @@ import pygments
 import re
 import sublime
 import sublime_plugin
-import sys
 import tempfile
 
-# crazy ST2 bug, see http://www.sublimetext.com/forum/viewtopic.php?f=6&t=1278
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)),
-    'pygments'))
+from pygments import highlight
+from pygments.lexers import *
+from pygments.formatters import *
+from pygments.styles import STYLE_MAP
 
-from pygments.lexers import (get_lexer_by_name, get_lexer_for_filename,
-    guess_lexer)
-from pygments.formatters import get_formatter_by_name
-from pygments.util import ClassNotFound
+# Don't judge me. Just don't. If you knew, you wouldn't.
+for s in STYLE_MAP:
+    __import__('pygments.styles.%s' % s)
 
 
+DEFAULT_STYLE = "default"
 FORMATS = ('html', 'rtf',)
 HTML_TEMPLATE = u"""<!DOCTYPE html>
 <html>
@@ -34,9 +33,16 @@ HTML_TEMPLATE = u"""<!DOCTYPE html>
 %(highlighted)s
 """
 
+settings = sublime.load_settings('%s.sublime-settings' % __name__)
+
 
 def get_template(**kwargs):
-    return kwargs.get('template', HTML_TEMPLATE) % dict(**kwargs)
+    if 'template' in kwargs:
+        template = kwargs['template']
+        del kwargs['template']
+    else:
+        template = HTML_TEMPLATE
+    return template % dict(**kwargs)
 
 
 class SublimeHighlightCommand(sublime_plugin.TextCommand):
@@ -71,29 +77,32 @@ class SublimeHighlightCommand(sublime_plugin.TextCommand):
         if not match:
             return
         try:
-            return get_lexer_by_name(match.group(1).lower())
-        except ClassNotFound:
+            return pygments.lexers.get_lexer_by_name(match.group(1).lower())
+        except pygments.util.ClassNotFound:
             return
 
-    def get_formatter(self, output_type, style="vim", full=True):
-        return get_formatter_by_name(output_type, style=style, full=full)
+    def get_formatter(self, output_type, full=True):
+        return pygments.formatters.get_formatter_by_name(output_type,
+            style=settings.get('theme', 'default'),
+            full=settings.get('full', True))
 
     def get_lexer(self, code=None):
         code = code if code is not None else self.code
         lexer = None
         if self.view.file_name():
             try:
-                lexer = get_lexer_for_filename(self.view.file_name(), code)
-            except ClassNotFound:
+                lexer = pygments.lexers.get_lexer_for_filename(
+                    self.view.file_name(), code)
+            except pygments.util.ClassNotFound:
                 pass
         if not lexer:
             lexer = self.guess_lexer_from_syntax()
         if not lexer:
-            lexer = guess_lexer(code)
+            lexer = pygments.lexers.guess_lexer(code)
         return lexer
 
     def highlight(self, output_type):
-        return pygments.highlight(self.code, self.get_lexer(),
+        return highlight(self.code, self.get_lexer(),
             self.get_formatter(output_type))
 
     def run(self, edit, target='external', output_type='html'):
@@ -111,7 +120,7 @@ class SublimeHighlightCommand(sublime_plugin.TextCommand):
             new_view = self.view.window().new_file()
             if output_type == 'html':
                 new_view.set_syntax_file('Packages/HTML/HTML.tmLanguage')
-            new_edit = new_view.begin_edit()
+            new_edit = new_vieiw.begin_edit()
             new_view.insert(new_edit, 0, pygmented)
             new_view.end_edit(new_edit)
         else:
